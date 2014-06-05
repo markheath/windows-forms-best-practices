e3d27d482bfb5b25808f96d8e91f58209917703e
Mark Heath <mark.heath@gmail.com>
2014-06-05 23:00:54 +0100

    basic rss parsing

--- PluralsightWinFormsDemoApp/Form1.cs
+++ PluralsightWinFormsDemoApp/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace PluralsightWinFormsDemoApp
 {
@@ -19,7 +21,25 @@ namespace PluralsightWinFormsDemoApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            var doc = new XmlDocument();
+            doc.Load("http://hwpod.libsyn.com/rss");
 
+            XmlElement channel = doc["rss"]["channel"];
+            XmlNodeList items = channel.GetElementsByTagName("item");
+            var title = channel["title"].InnerText;
+            var link = channel["link"].InnerText;
+            var description = channel["description"].InnerText;
+
+            foreach (var item in items)
+            {                
+                var podTitle = item["title"].InnerText;
+                var poddescription = item["description"].InnerText;
+                var podLink = item["link"].InnerText;
+                //this.items.Add(rssItem);
+                listBox2.Items.Add(podTitle);
+            }
+
+            // Rss20FeedFormatter didn't work
         }
     }
 }
