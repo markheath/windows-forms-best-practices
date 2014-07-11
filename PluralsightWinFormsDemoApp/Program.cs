11c350e0750362c0e62445d87ed48738ac66510d
Mark Heath <mark.heath@gmail.com>
2014-07-11 21:56:44 +0100

    adding global exception handler

--- PluralsightWinFormsDemoApp/Program.cs
+++ PluralsightWinFormsDemoApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,9 +17,39 @@ namespace PluralsightWinFormsDemoApp
         [STAThread]
         static void Main()
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += ApplicationOnThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var message = String.Format("Sorry, something went wrong.\r\n" +
+                "{0}\r\n" +
+                "Please contact support.",
+                ((Exception)e.ExceptionObject).Message);
+
+            Console.WriteLine("ERROR {0}: {1}",
+                DateTimeOffset.Now, e.ExceptionObject);
+
+            MessageBox.Show(message, "Unexpected Error");
+
+        }
+
+        private static void ApplicationOnThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            var message = String.Format("Sorry, something went wrong.\r\n" +
+              "{0}\r\n" +
+              "Please contact support.",
+                e.Exception.Message);
+
+            Console.WriteLine("ERROR {0}: {1}",
+                DateTimeOffset.Now, e.Exception);
+
+            MessageBox.Show(message, "Unexpected Error");
         }
     }
 }
