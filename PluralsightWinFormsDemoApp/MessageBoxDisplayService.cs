c1db714000326fdcce8f91dc4ed4655dd3346d05
Mark Heath <mark.heath@gmail.com>
2014-07-19 12:50:12 +0100

    adding missing service from last commit

---
+++ PluralsightWinFormsDemoApp/MessageBoxDisplayService.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace PluralsightWinFormsDemoApp
+{
+    internal class MessageBoxDisplayService : IMessageBoxDisplayService
+    {
+        public void Show(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+
+    internal interface IMessageBoxDisplayService
+    {
+        void Show(string message);
+    }
+}
